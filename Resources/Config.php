d91e4de0c7809947fa42217e48cbbd85d7635d6d
Rafael Carlos <xDark-Nightx>
2022-06-12 20:26:29 -0300

    start

---
+++ Resources/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$config = array(
+    "db" => array(
+        "remote" => array(
+            "username" => "rcsser31_system_hm",
+            "password" => "DC@k%J#,*{SH",
+            "host" => "162.241.203.46",
+            "connectionInfo" => "Database = rcsser31_house_manager"
+        )
+    ),
+    "urls" => array(
+        "baseUrl" => "https://house.rcsserver.com.br/"
+    )
+);
+
--- System/Controller/Database.php
+++ System/Controller/Database.php
@@ -1,1 +1,51 @@
 <?php
+
+defined('__PROJECT_DIR__') or define('__PROJECT_DIR__', $_SERVER['DOCUMENT_ROOT']);
+require_once(__PROJECT_DIR__."/resources/config.php");
+
+class Database{
+    private PDO $mConnection;
+
+    private function Connect() : bool{
+        try {
+            if (!isset($config['db']['sql'])){
+                return false;
+            }
+
+            $credentials = $config['db']['sql'];
+
+            $this->mConnection = new PDO("sqlsrv:server = {$credentials['host']} ; {$credentials['connectionInfo']}",
+                $credentials['username'], $credentials['password']);
+
+            return true;
+        } catch (PDOException $exception) {
+            print "[Could not connect to SQL Server] -> error: {$exception->getMessage()}\n";
+            return false;
+        }
+    }
+
+    function Query($query, $fetchAll = true){
+        if (!$this->Connect()){
+            return false;
+        }
+
+        try{
+            $stmt = $this->mConnection->query($query);
+
+            if ($stmt === false){
+                return false;
+            }
+
+            if ($fetchAll){
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            return $result;
+        } catch (Exception $exception){
+            print "[Could not execute query] -> error: {$exception->getMessage()}\n";
+            return false;
+        }
+    }
+}
